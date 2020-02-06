ceacbaa5bec9ab6094823572bfe32ea1d78e9950
vgrados <vgrados2@outlook.com>
2020-02-06 02:20:06 -0500

    proyecto inicio branch

--- engine.js
+++ engine.js
@@ -4,23 +4,19 @@ const map = require('lodash.map');
 const longest = require('longest');
 const rightPad = require('right-pad');
 const chalk = require('chalk');
-const git = require('simple-git')
+const git = require('simple-git');
+const { spawnSync } = require('child_process');
 
-const filterSubject = function(subject) {
-  subject = subject.trim();
-  if (subject.charAt(0).toLowerCase() !== subject.charAt(0)) {
-    subject =
-      subject.charAt(0).toLowerCase() + subject.slice(1, subject.length);
-  }
-  while (subject.endsWith('.')) {
-    subject = subject.slice(0, subject.length - 1);
-  }
-  return subject;
+const transformInput = function(input) {
+  input = input.trim();
+  return input.toUpperCase();
 };
 
-// This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just
-// fine.
+function hasWhiteSpace(value) {
+  const inValid = /\s/;
+  return inValid.test(value) ? 'No se permiten espacios en blanco' : true;
+}
+
 module.exports = function(options) {
   const types = options.types;
   const length = longest(Object.keys(types)).length + 1;
@@ -32,83 +28,89 @@ module.exports = function(options) {
   });
 
   return {
-    // When a user runs `git cz`, prompter will
-    // be executed. We pass you cz, which currently
-    // is just an instance of inquirer.js. Using
-    // this you can ask questions and get answers.
-    //
-    // The commit callback should be executed when
-    // you're ready to send back a commit template
-    // to git.
-    //
-    // By default, we'll de-indent your commit
-    // template and will keep empty lines.
-    prompter: function(cz, commit) {
-      // Let's ask some questions of the user
-      // so that we can populate our commit
-      // template.
-      //
-      // See inquirer.js docs for specifics.
-      // You can also opt to use another input
-      // collection library if you prefer.
+    prompter: function(cz) {
       cz.prompt([
         {
           type: 'list',
           name: 'type',
-          message: "Seleccione el tipo de cambio que está por realizar: \n",
+          message: 'Seleccione el tipo de cambio que está por realizar: \n',
           choices: choices,
-          default: 'refactor',
+          default: 'refactor'
         },
         {
           type: 'input',
           name: 'name',
           message: function() {
-            return (
-              'Escriba un nombre para su rama:\n'
-            );
+            return 'Escriba un nombre para su rama:\n';
           },
           validate: function(subject) {
-            const filteredSubject = filterSubject(subject);
-            return filteredSubject.length === 0
-              ? 'Nombre de la rama es requerido'
-              : true;
+            const filteredSubject = transformInput(subject);
+            if (filteredSubject.length === 0) {
+              return 'Nombre de la rama es requerido';
+            } else if (filteredSubject.length < 5) {
+              return 'El Minimo de caracteres permitido es 5';
+            } else {
+              return hasWhiteSpace(subject);
+            }
           },
           transformer: function(subject) {
-            const filteredSubject = filterSubject(subject);
-            const color = subject.length === 0 ? chalk.red : chalk.green;
-            return color('(' + filteredSubject.length + ') ' + subject);
-          },
-          filter: function(subject) {
-            return filterSubject(subject);
+            const nameInput = transformInput(subject);
+            const color =
+              subject.length === 0 || subject.length < 5
+                ? chalk.red
+                : chalk.cyan;
+            return color(nameInput);
           }
         },
         {
           type: 'confirm',
           name: 'jiraAffected',
           message: 'Esta rama esta asociada a una historia de JIRA?',
-          default: !!options.defaultIssues
+          default: false
         },
         {
           type: 'input',
           name: 'jiraHistory',
           message: '(Ejemplo: "ASSIAC-123", "CAP-123".):\n',
+          validate: function(subject) {
+            const HISTORY_JIRA_FORMAT = /^(ASSIAC|CAP)-[0-9]{1,5}/;
+            return !HISTORY_JIRA_FORMAT.test(subject)
+              ? 'Formato de historia invalido'
+              : hasWhiteSpace(subject);
+          },
+          transformer: function(subject) {
+            return transformInput(subject);
+          },
           when: function(answers) {
             return answers.jiraAffected;
-          },
-          default: options.defaultIssues ? options.defaultIssues : undefined
+          }
         }
       ]).then(function(answers) {
-        const wrapOptions = {
-          trim: true,
-          cut: false,
-          newline: '\n',
-          indent: '',
-          width: options.maxLineWidth
-        };
-        // feature/CPV-3925#BUSQUEDA-RUC-REPORTES
-        const jiraHistory = answers.jiraAffected ? `${answers.jiraHistory}#` : '';
+        const child = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+        const activeBranchName = child.stdout.toString().trim();
+        const jiraHistory = answers.jiraAffected
+          ? `${answers.jiraHistory}#`
+          : '';
         const branchName = `${answers.type}/${jiraHistory}${answers.name}`;
-
+        git().checkoutBranch(branchName, activeBranchName, (err, data) => {
+          if (!err) {
+            console.log(
+              chalk.yellow(
+                `-------------------------------------------------------------`
+              )
+            );
+            console.log(
+              chalk.yellow(`NOMBRE DE RAMA BASE: ${activeBranchName}`)
+            );
+            console.log(chalk.yellow(`NOMBRE DE RAMA GENERADA: ${branchName}`));
+            console.log(
+              chalk.yellow(
+                `-------------------------------------------------------------`
+              )
+            );
+            console.log(chalk.green(`proceso finalizado.`));
+          }
+        });
       });
     }
   };
--- index.js
+++ index.js
@@ -2,47 +2,7 @@
 
 const engine = require('./engine');
 const conventionalCommitTypes = require('./commons/types');
-const configLoader = require('commitizen').configLoader;
-
-var config = configLoader.load();
-var options = {
-  types: conventionalCommitTypes.types,
-  defaultType: 'Utilice las teclas de dirección',
-  defaultScope: process.env.CZ_SCOPE || config.defaultScope,
-  defaultSubject: process.env.CZ_SUBJECT || config.defaultSubject,
-  defaultBody: process.env.CZ_BODY || config.defaultBody,
-  defaultIssues: process.env.CZ_ISSUES || config.defaultIssues,
-  disableScopeLowerCase:
-    process.env.DISABLE_SCOPE_LOWERCASE || config.disableScopeLowerCase,
-  maxHeaderWidth:
-    (process.env.CZ_MAX_HEADER_WIDTH &&
-      parseInt(process.env.CZ_MAX_HEADER_WIDTH)) ||
-    config.maxHeaderWidth ||
-    100,
-  maxLineWidth:
-    (process.env.CZ_MAX_LINE_WIDTH &&
-      parseInt(process.env.CZ_MAX_LINE_WIDTH)) ||
-    config.maxLineWidth ||
-    100
+const options = {
+  types: conventionalCommitTypes.types
 };
-
-(function(options) {
-  try {
-    var commitlintLoad = require('@commitlint/load');
-    commitlintLoad().then(function(clConfig) {
-      if (clConfig.rules) {
-        var maxHeaderLengthRule = clConfig.rules['header-max-length'];
-        if (
-          typeof maxHeaderLengthRule === 'object' &&
-          maxHeaderLengthRule.length >= 3 &&
-          !process.env.CZ_MAX_HEADER_WIDTH &&
-          !config.maxHeaderWidth
-        ) {
-          options.maxHeaderWidth = maxHeaderLengthRule[2];
-        }
-      }
-    });
-  } catch (err) {}
-})(options);
-
 module.exports = engine(options);
